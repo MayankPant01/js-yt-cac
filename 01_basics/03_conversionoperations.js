d1a863a0b1ba03f86e15200f06edd6fe300add67
MayankPant01 <mayankpant2801@gmail.com>
2025-11-18 04:56:07 +0000

    conversionop

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,14 @@
+let score = "Mayank"
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = "Mayank"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+let someNumber=33;
+let stringNumber = String(someNumber)
+
