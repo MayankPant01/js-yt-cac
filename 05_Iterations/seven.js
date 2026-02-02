ddf66dc3a848df74661f3ef04cb2dc0815c6b87d
MayankPant01 <mayankpant2801@gmail.com>
2026-02-02 07:00:57 +0000

    map_concept

---
+++ 05_Iterations/seven.js
@@ -0,0 +1,26 @@
+// //Map
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+const n = []
+
+// for(let i in myNumbers){
+//     let m = myNumbers[i]
+//     m = m+10
+//     n.push(m)
+// }
+
+// console.log(n)
+
+// // Using Map
+
+// const newNums = myNumbers.map((num)=> num +10)
+// console.log(newNums)
+
+// //Using forEach
+// myNumbers.forEach((num)=> {
+//     let a = num+10
+//     n.push(a)
+
+// })
+
+// console.log(n)
